a8c01060632365da2b0242ff939c5ef774da104c
bhmossberg <benjamin.h.mossberg@gmail.com>
2026-07-22 20:31:13 -0400

    v1.0

--- src/module1/Bag.java
+++ src/module1/Bag.java
@@ -1,9 +1,55 @@
 package module1;
 
-public class Bag {
+import java.util.ArrayList;
+import java.util.Objects;
+/**
+ * A generic Bag (multiset) implementation that allows duplicate elements.
+ * Internally uses an ArrayList to store items.
+ *
+ * @param <T> the type of elements stored in the bag
+ */
+public class Bag<T>{
+	
+	private final ArrayList<T> items;
 
 	public Bag() {
-		// TODO Auto-generated constructor stub
+		this.items = new ArrayList<>();
 	}
+	
+	// Returns string representation of bag contents
+	@Override
+	public String toString() {
+		return items.toString();
+	}
+	
+	// Method adds an item of type T to the bag
+	public void add(T item) {
+		items.add(item);
+	}
+	
+	// Method removes one occurrence of the item from the bag, if it exists
+	public void remove(T item) {
+		items.remove(item);
+	}
+	
+	// Method returns true if the item exists in the bag; otherwise, returns false.   
+	public boolean contains(T item) {
+		return items.contains(item);
+	}
+	
+	/* Method returns the number of occurrences of the specified item in the bag. 
+	 * If the item does not exist, it returns 0.
+	 */
+	public int count(T item) {
+		int frequency = 0;
+		for (T element : items) {
+			if (Objects.equals(element, item)) {
+				frequency++;
+			}
+		}
+		return frequency;
+	}
+
+
 
 }
--- src/module1/Main.java
+++ src/module1/Main.java
@@ -2,13 +2,38 @@ package module1;
 
 public class Main {
 
-	public Main() {
-		// TODO Auto-generated constructor stub
-	}
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Bag<String> guitarBag = new Bag<>();
+		
+		// Add sample items to bag
+		guitarBag.add("Gibson SG");
+		guitarBag.add("Gibson Les Paul");
+		guitarBag.add("Gibson ES-335");
+		guitarBag.add("Gibson SG");
+		guitarBag.add("Fender Stratocaster");
+		guitarBag.add("Gretsch White Falcon");
+		guitarBag.add("Fender Telecaster");
+		
+		// Print bag contents
+		System.out.println("Bag contents:\n" + guitarBag.toString() + "\n");
+		
+		// Test contains()
+		System.out.println("Contains \"Gibson SG\"?         " + guitarBag.contains("Gibson SG"));
+		System.out.println("Contains \"Fender Telecaster\"? " + guitarBag.contains("Fender Telecaster"));
+		System.out.println("Contains \"Ibanez JEM\"?        " + guitarBag.contains("Ibanez JEM") + "\n");
+		
+		// Test count()
+		System.out.println("Count of \"Gibson SG\":         " + guitarBag.count("Gibson SG"));
+		System.out.println("Count of \"Fender Telecaster\": " + guitarBag.count("Fender Telecaster"));
+		System.out.println("Count of \"Ibanez JEM\":        " + guitarBag.count("Ibanez JEM") + "\n");
+		
+		// Test remove()
+		guitarBag.remove("Fender Telecaster");
+		System.out.println("Bag contents after removing one \"Fender Telecaster\":\n" + guitarBag);
+		
+		// Test contains() and count() for the removed element
+		System.out.println("Contains \"Fender Telecaster\"? " + guitarBag.contains("Fender Telecaster"));
+		System.out.println("Count of \"Fender Telecaster\": " + guitarBag.count("Fender Telecaster"));
 	}
 
 }
